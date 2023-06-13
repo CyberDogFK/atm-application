7acd21b936bb5ef992176314468ff1617f616d67
Anton Pavliuk <antonpavliuk@Antons-MacBook-Air.local>
2023-06-13 17:15:05 +0300

    Add implementation of adding money into account from atm

--- src/main/java/com/antp/atmapplication/controller/AtmController.java
+++ src/main/java/com/antp/atmapplication/controller/AtmController.java
@@ -1,11 +1,15 @@
 package com.antp.atmapplication.controller;
 
-import com.antp.atmapplication.dto.AtmRequestDto;
-import com.antp.atmapplication.dto.AtmResponseDto;
+import com.antp.atmapplication.dto.*;
 import com.antp.atmapplication.exception.DataProcessingException;
+import com.antp.atmapplication.model.Account;
 import com.antp.atmapplication.model.Atm;
 import com.antp.atmapplication.model.AtmBalance;
+import com.antp.atmapplication.model.User;
+import com.antp.atmapplication.service.AccountService;
+import com.antp.atmapplication.service.AtmBalanceService;
 import com.antp.atmapplication.service.AtmService;
+import com.antp.atmapplication.service.UserService;
 import com.antp.atmapplication.service.mapper.RequestDtoMapper;
 import com.antp.atmapplication.service.mapper.ResponseDtoMapper;
 import java.math.BigDecimal;
@@ -14,6 +18,7 @@ import java.util.List;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,15 +33,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class AtmController {
     private final static Logger logger = LoggerFactory.getLogger(AtmController.class);
     private final AtmService atmService;
+    private final UserService userService;
+    private final AccountService accountService;
+    private final AtmBalanceService atmBalanceService;
     private final ResponseDtoMapper<AtmResponseDto, Atm> atmResponseDtoMapper;
     private final RequestDtoMapper<AtmRequestDto, Atm> atmRequestDtoMapper;
+    private final ResponseDtoMapper<AtmBalanceResponseDto, AtmBalance> atmBalanceResponseDtoMapper;
+    private final ResponseDtoMapper<AccountResponseDto, Account> accountResponseDtoMapper;
 
     public AtmController(AtmService atmService,
-                         ResponseDtoMapper<AtmResponseDto, Atm> atmResponseDtoMapper,
-                         RequestDtoMapper<AtmRequestDto, Atm> atmRequestDtoMapper) {
+                         UserService userService, AccountService accountService, AtmBalanceService atmBalanceService, ResponseDtoMapper<AtmResponseDto, Atm> atmResponseDtoMapper,
+                         RequestDtoMapper<AtmRequestDto, Atm> atmRequestDtoMapper,
+                         ResponseDtoMapper<AtmBalanceResponseDto, AtmBalance> atmBalanceResponseDtoMapper, ResponseDtoMapper<AccountResponseDto, Account> accountResponseDtoMapper) {
         this.atmService = atmService;
+        this.userService = userService;
+        this.accountService = accountService;
+        this.atmBalanceService = atmBalanceService;
         this.atmResponseDtoMapper = atmResponseDtoMapper;
         this.atmRequestDtoMapper = atmRequestDtoMapper;
+        this.atmBalanceResponseDtoMapper = atmBalanceResponseDtoMapper;
+        this.accountResponseDtoMapper = accountResponseDtoMapper;
     }
 
     @GetMapping
@@ -81,5 +97,41 @@ public class AtmController {
         logger.info("atm balance before" + balance);
         atmBalance.setBalance(balance.add(new BigDecimal(value)));
         return atmResponseDtoMapper.mapToDto(atmService.save(byId));
+    }
+
+    @GetMapping("/{id}/balances")
+    public List<AtmBalanceResponseDto> getAtmBalances(@PathVariable Long id) {
+        return atmService.getById(id)
+                .getBalanceList().stream()
+                .map(atmBalanceResponseDtoMapper::mapToDto)
+                .toList();
+    }
+
+    @PutMapping("/{id}/account/{accountId}")
+    @Transactional
+    public AccountResponseDto putMoneyInAccount(@PathVariable Long id,
+                                                @PathVariable Long accountId,
+                                                @RequestParam String currency,
+                                                @RequestParam BigDecimal value,
+                                                Authentication authentication) {
+        User user = userService.findByName(authentication.getName()).orElseThrow(() ->
+                new RuntimeException("Can't find your account with name "
+                        + authentication.getName()));
+        Account account = user.getAccounts().stream()
+                .filter(it -> it.getId().equals(accountId))
+                .findFirst().orElseThrow(() ->
+                        new RuntimeException("Can't find user: " + user.getName()
+                                + " account with id " + accountId));
+        Atm atm = atmService.getById(id);
+        AtmBalance atmBalance = atm.getBalanceList().stream()
+                .filter(it -> it.getCurrency().getShortName().equals(currency))
+                .findFirst().orElseThrow(() ->
+                        new RuntimeException("Atm with id " + atm.getId()
+                                + " have only currency " + atm.getBalanceList()));
+        account.setBalance(account.getBalance().add(value));
+        Account save = accountService.save(account);
+        atmBalance.setBalance(atmBalance.getBalance().add(value));
+        atmBalanceService.save(atmBalance);
+        return accountResponseDtoMapper.mapToDto(save);
     }
 }
