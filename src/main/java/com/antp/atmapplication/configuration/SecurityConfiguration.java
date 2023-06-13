eb37e8f07dcf4c336102dbd8ce821f4d43b980d1
Anton Pavliuk <antonpavliuk@Antons-MacBook-Air.local>
2023-06-13 11:26:29 +0300

    Configure access to h2-database when security is enables

--- src/main/java/com/antp/atmapplication/configuration/SecurityConfiguration.java
+++ src/main/java/com/antp/atmapplication/configuration/SecurityConfiguration.java
@@ -3,16 +3,20 @@ package com.antp.atmapplication.configuration;
 import com.antp.atmapplication.security.jwt.JwtConfigurer;
 import com.antp.atmapplication.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -32,6 +36,11 @@ public class SecurityConfiguration {
         auth.userDetailsService(detailsService).passwordEncoder(passwordEncoder);
     }
 
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().requestMatchers(PathRequest.toH2Console()).and();
+//    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -40,8 +49,10 @@ public class SecurityConfiguration {
                 .and()
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers(HttpMethod.POST,"/register", "/login").permitAll()
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().permitAll()
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+//                        .requestMatchers("/h2-console").permitAll()
+//                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .csrf().disable()
                 .headers().frameOptions().disable()
